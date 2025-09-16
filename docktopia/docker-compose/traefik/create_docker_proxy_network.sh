c74bc43efd6a16b35f4f65515836a5d0da871c0f
nimblebytes <19956346+nimblebytes@users.noreply.github.com>
2025-09-16 11:57:17 +0200

    Updated script to be easier to read

--- docktopia/docker-compose/traefik/create_docker_proxy_network.sh
+++ docktopia/docker-compose/traefik/create_docker_proxy_network.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-docker network create --driver bridge --gateway 172.27.0.1 --subnet 172.27.0.0/24 proxy
-# docker network create --driver bridge --internal --gateway 172.27.0.1 --subnet 172.27.0.0/24 proxy
-# docker network create --driver bridge --internal --gateway 172.27.0.1 --subnet 172.27.1.0/24 proxy_nointernet
+NETWORK_NAME=proxy
+
+## Create a network to connect service that needed to be proxied. Bridge network will have internet access.
+docker network create \ 
+  --driver bridge \ 
+  --subnet=172.27.0.0/24 \ 
+  --gateway=172.27.0.1 \ 
+  --ip-range=172.27.0.0/24 \ 
+  # --aux-address="DEVICE_NAME=172.27.0.2" \                  ## How to define an address already in use. NOT for reserving an IP
+  --label "com.docker.compose.network=${NETWORK_NAME}" \ 
+  --label "internal.docker.network.description=Proxy network for container services (${HOSTNAME:?})" \ 
+${NETWORK_NAME} 
